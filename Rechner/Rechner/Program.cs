1bd80758ffd43c4dcaa8013113fc6dd504f70376
AdrianMartinezJS <adrian.pdsm@gmail.com>
2021-10-22 21:59:10 +0200

    20211022

--- Rechner/Rechner/Program.cs
+++ Rechner/Rechner/Program.cs
@@ -7,61 +7,135 @@ namespace Rechner
         static void Main(string[] args)
         {
 
-            // Definition Variablen
-            double ersterSummando, zweiterSummando, result;
-
             // Hol Zahlen vom User
-            string ersterZahl = HolZahl("Geben Sie der erster Zahl ein: ");
-            string zweiterZahl = HolZahl("Geben Sie der zweiter Zahl ein: ");
-            string Operator = HolZahl("Was möchten Sie mit dieser Zahlen machen? Addieren (+), Subtrahieren (-), Multiplizieren (*) oder dividieren (/): ");
-
+            string ersterZahlAlsString = HolBenutzerEingabe("Geben Sie die erste Zahl ein: ");
+            string operation = HolBenutzerEingabe("Welche Berechnung möchten Sie machen? Addieren (+), Subtrahieren (-), Multiplizieren (*) oder dividieren (/): ");
+            string zweiterZahlAlsString = HolBenutzerEingabe("Geben Sie die zweite Zahl ein: ");
+            
             // Wandel Text in Zahlen
-            ersterSummando = Convert.ToDouble(ersterZahl);
-            zweiterSummando = Convert.ToDouble(zweiterZahl);
+            // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist
+            double ersterZahlAlsInt = Convert.ToDouble(ersterZahlAlsString);
+            double zweiterZahlAlsInt = Convert.ToDouble(zweiterZahlAlsString);
 
             // Berechnung ausführen
-            result = Operation(ersterSummando, zweiterSummando, Operator);
+            double result = Berechnung(ersterZahlAlsInt, zweiterZahlAlsInt, operation);
 
             // Ausgabe der Summe in der Console
-            Console.WriteLine("Die Berechnung lautet: {0}", result);
-            WarteAufBenutzerEingabe();
+            GibResultatAus(result, operation);
+            HolBenutzerEingabe("Drucken Sie eine beliebige Taste um das Programm zu beenden. ");
         }
 
         /// <summary>
-        /// Macht die Operation
+        /// Gib das Resultat in der Console aus Anhand der Operator und das Resultat
         /// </summary>
-        /// <param name="Nummer">Erster zahl vom User eingegeben</param>
-        /// <param name="Nummer2">Zweiter zahl vom User eingegeben</param>
-        /// <param name="Operator">Operator Symbol ("+-*/")</param>
-        static double Operation(double Nummer, double Nummer2, string Operator)
+        /// <param name="resultat">Das Resultat aus der Berechnung Funktion</param>
+        /// <param name="operation">Das Operator was dem User eingegeben hat</param>
+        static void GibResultatAus(double resultat, string operation)
         {
-            switch (Operator)
+            switch (operation)
             {
                 case "+":
-                    return Nummer + Nummer2;
+                    Console.WriteLine("Die Summe ist: {0}", resultat);
+                    break;
 
                 case "-":
-                    return Nummer - Nummer2;
+                    Console.WriteLine("Die Differenz ist: {0}", resultat);
+                    break;
 
                 case "*":
-                    return Nummer * Nummer2;
+                    Console.WriteLine("Das Produkt ist: {0}", resultat);
+                    break;
 
                 case "/":
-                    return Nummer / Nummer2;
+                    Console.WriteLine("Der Wert des Quotienten ist: {0}", resultat);
+                    break;
 
                 default:
-                    return 0;
+                    Console.WriteLine("Es ist etwas schief gelaufen..");
+                    break;
             }
         }
 
-        /// <summary>
-        /// Sagt den User was er tun muss um den Programm zu beenden
-        /// Beendet das Program
-        /// </summary>
-        static void WarteAufBenutzerEingabe()
+        ///// <summary>
+        ///// Macht die Operation
+        ///// </summary>
+        ///// <param name="Nummer">Erster zahl vom User eingegeben</param>
+        ///// <param name="Nummer2">Zweiter zahl vom User eingegeben</param>
+        ///// <param name="Operator">Operator Symbol ("+-*/")</param>
+        static double Berechnung(double Nummer, double Nummer2, string operation)
         {
-            Console.WriteLine("Drucken Sie eine beliebige Taste um das Programm zu beenden.");
-            Console.ReadLine();
+            double resultat = 0;
+            switch (operation)
+            {
+                case "+":
+                    resultat = Addiere(Nummer, Nummer2);
+                    break;
+
+                case "-":
+                    resultat = Subtrahiere(Nummer, Nummer2);
+                    break;
+
+                case "*":
+                    resultat = Multipliziere(Nummer, Nummer2);
+                    break;
+
+                case "/":
+                    resultat = Dividiere(Nummer, Nummer2);
+                    break;
+            }
+            return resultat;
+        }
+
+        /// <summary>
+        /// Subtrahiert 2 vorgegebene Zahlen
+        /// </summary>
+        /// <param name="Nummer">Erster Zahl vom User</param>
+        /// <param name="Nummer2">Zweiter Zahl vom User</param>
+        /// <returns></returns>
+        static double Subtrahiere(double Nummer, double Nummer2)
+        {
+            double sub = Nummer - Nummer2;
+
+            return sub;
+        }
+
+        /// <summary>
+        /// Addiert 2 vorgegebene Zahlen
+        /// </summary>
+        /// <param name="Nummer">Erster Zahl vom User</param>
+        /// <param name="Nummer2">Zweiter Zahl vom User</param>
+        /// <returns></returns>
+        static double Addiere(double Nummer, double Nummer2)
+        {
+            double add = Nummer + Nummer2;
+
+            return add;
+        }
+
+        /// <summary>
+        /// Multipliziert 2 vorgegebene Zahlen
+        /// </summary>
+        /// <param name="Nummer">Erster Zahl vom User</param>
+        /// <param name="Nummer2">Zweiter Zahl vom User</param>
+        /// <returns></returns>
+        static double Multipliziere(double Nummer, double Nummer2)
+        {
+            double mult = Nummer * Nummer2;
+
+            return mult;
+        }
+
+        /// <summary>
+        /// Dividiert 2 vorgegebene Zahlen
+        /// </summary>
+        /// <param name="Nummer">Erster Zahl vom User</param>
+        /// <param name="Nummer2">Zweiter Zahl vom User</param>
+        /// <returns></returns>
+        static double Dividiere(double Nummer, double Nummer2)
+        {
+            double div = Nummer / Nummer2;
+
+            return div;
         }
 
         /// <summary>
@@ -69,7 +143,7 @@ namespace Rechner
         /// </summary>
         /// <param name="ausgabeText">Was wird in der Console für den User geschrieben</param>
         /// <returns></returns>
-        static string HolZahl( string ausgabeText)
+        static string HolBenutzerEingabe( string ausgabeText, double resultat = 0)
         {
             Console.WriteLine(ausgabeText);
             string zahl = Console.ReadLine();
