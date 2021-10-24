4c2655467687ddc758a7b1e5c19aae518a8b7416
AdrianMartinezJS <adrian.pdsm@gmail.com>
2021-10-24 23:02:46 +0200

    20211024 While & For Schleifen

--- DatentypFloat/DatentypFloat/Program.cs
+++ DatentypFloat/DatentypFloat/Program.cs
@@ -10,23 +10,70 @@ namespace DatentypFloat
     {
         static void Main(string[] args)
         {
-            float pi = 3.14F;
-            Console.WriteLine("Unser pi hat den Wert: {0}", pi);
-            float pi2 = 0.0314e2F; // 0,0314 * 10 hoch 2
-            Console.WriteLine("Unser pi2 hat den Wert: {0}", pi2);
+            //float pi = 3.14F;
+            //Console.WriteLine("Unser pi hat den Wert: {0}", pi);
+            //float pi2 = 0.0314e2F; // 0,0314 * 10 hoch 2
+            //Console.WriteLine("Unser pi2 hat den Wert: {0}", pi2);
 
-            float max = float.MaxValue;
-            Console.WriteLine("Ein float hat auf diesem Rechner den maximal Wert (ohne :F): {0}", max);
-            Console.WriteLine("Ein float hat auf diesem Rechner den maximal Wert (mit :F): {0:F}", max); // :F heißt dass man nicht den exponentiallen Wert sieht, sondern den richtigen Zahl
+            //float max = float.MaxValue;
+            //Console.WriteLine("Ein float hat auf diesem Rechner den maximal Wert (ohne :F): {0}", max);
+            //Console.WriteLine("Ein float hat auf diesem Rechner den maximal Wert (mit :F): {0:F}", max); // :F heißt dass man nicht den exponentiallen Wert sieht, sondern den richtigen Zahl
 
-            // Einen Float hat genau 10 Stellen genauigkeit nach den Kommazahl!!
-            float a = 123456789.01F;
-            float b = 123456789.02F;
-            Console.WriteLine("a hat den Wert: {0}", a);
-            Console.WriteLine("b hat den Wert: {0}", b);
+            //// Einen Float hat genau 10 Stellen genauigkeit nach den Kommazahl!!
+            //float a = 123456789.01F;
+            //float b = 123456789.02F;
+            //Console.WriteLine("a hat den Wert: {0}", a);
+            //Console.WriteLine("b hat den Wert: {0}", b);
 
-            // Einen double hat 16 Stellen genauigkeit
+            //// Einen double hat 16 Stellen genauigkeit
+            //Console.ReadLine();
+            EinMehrFachEcho();
+            Console.WriteLine("Betätige RETURN zum beenden");
             Console.ReadLine();
+
+        }
+
+        static void EinfachesEcho()
+        {
+            string eingabe = "";
+
+            Console.WriteLine("Du wirst gleich ein Echo sehen!");
+
+            Console.WriteLine("Sag etwas ;)");
+            eingabe = Console.ReadLine();
+
+            Console.WriteLine(eingabe);
+        }
+
+        static void ImmerWiederEcho()
+        {
+            string eingabe = "";
+
+            Console.WriteLine("Du wirst gleich ein Echo sehen!");
+            Console.WriteLine("Zum beenden ENDE eingeben.");
+
+            while (eingabe != "ENDE")
+            {
+                Console.WriteLine("Sag etwas ;)");
+                eingabe = Console.ReadLine();
+
+                Console.WriteLine(eingabe);
+            }
+        }
+
+        static void EinMehrFachEcho()
+        {
+            string eingabe = "";
+
+            Console.WriteLine("Du wirst gleich ein Echo sehen!");
+            Console.WriteLine("Sag etwas ;)");
+            eingabe = Console.ReadLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(eingabe);
+            }
+            
         }
     }
 }
